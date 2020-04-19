8ebce49d06a6614f3223b7ddc848000409bf43f8
Andreas Krinke <andreas.krinke@gmx.de>
2020-04-19 20:20:13 +0200

    Improve texts.
     - add space after "Update:" at the top left
     - improve setup instructions at the bottom left

--- text.h
+++ text.h
@@ -20,8 +20,8 @@ See more at http://blog.squix.ch
 
 #if LANG == 'EN'
   const char TEXT_REFRESH_BUTTON[] PROGMEM     = "REFRESH";
-  const char TEXT_CONFIG_BUTTON[] PROGMEM      = "CONFIG + RST";
-  const char TEXT_UPDATED[] PROGMEM            = "Updated:";
+  const char TEXT_CONFIG_BUTTON[] PROGMEM      = "Setup: Hold left and press right";
+  const char TEXT_UPDATED[] PROGMEM            = "Updated: ";
   const char TEXT_SUN[] PROGMEM                = "Sun:";
   const char TEXT_MOON[] PROGMEM               = "Moon:";
   const String WEEKDAYS[]                      = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
@@ -29,8 +29,8 @@ See more at http://blog.squix.ch
                                                   "Full Moon", "Waning Gibbous", "Third quarter", "Waning Crescent"};
 #elif LANG == 'DE'
   const char TEXT_REFRESH_BUTTON[] PROGMEM     = "AKTUALISIEREN";
-  const char TEXT_CONFIG_BUTTON[] PROGMEM      = "CONFIG + RST";
-  const char TEXT_UPDATED[] PROGMEM            = "Aktualisiert:";
+  const char TEXT_CONFIG_BUTTON[] PROGMEM      = "Setup: Links halten + rechts kurz drücken";
+  const char TEXT_UPDATED[] PROGMEM            = "Aktualisiert: ";
   const char TEXT_SUN[] PROGMEM                = "Sonne:";
   const char TEXT_MOON[] PROGMEM               = "Mond:";
   const String WEEKDAYS[]                      = {"SO", "MO", "DI", "MI", "DO", "FR", "SA"};
@@ -38,11 +38,11 @@ See more at http://blog.squix.ch
                                                   "Vollmond", "drittes Viertel", "abnehmender Mond", "letztes Viertel"};
 #elif LANG == 'FR'
   const char TEXT_REFRESH_BUTTON[] PROGMEM     = "ACTUALISER";
-  const char TEXT_CONFIG_BUTTON[] PROGMEM      = "CONFIG + RST";
-  const char TEXT_UPDATED[] PROGMEM            = "Mise à jour ";
+  const char TEXT_CONFIG_BUTTON[] PROGMEM      = "Setup: Tenir à gauche et presser à droite";
+  const char TEXT_UPDATED[] PROGMEM            = "Mise à jour: ";
   const char TEXT_SUN[] PROGMEM                = "Soleil:";
   const char TEXT_MOON[] PROGMEM               = "Lune:";
   const String WEEKDAYS[]                      = {"DI", "LU", "MA", "ME", "JE", "VE", "SA"};
   const String MOON_PHASES[]                   = {"nouvelle lune", "premier croissant", "premier quartier", "lune croissante",
-                                                  "pleine lune", "lune décroissante", "dernier quartier", "dernier croissant"};  
+                                                  "pleine lune", "lune décroissante", "dernier quartier", "dernier croissant"};
 #endif
